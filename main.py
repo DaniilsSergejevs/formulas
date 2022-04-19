2b1cb3c63d96afe5aa3a1aa4f7e964317e8c8e31
Daniil Sergejev <77155713+DaniilsSergejevs@users.noreply.github.com>
2022-04-19 09:52:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import math   
+number = int (input("Ievadi Skaitli: ", ))
+print("Faktorials būs", math.factorial(number))
+
+print(math.fmod(5,2))  
+print(math.fmod(-5,2))  
+print(math.fmod(-5.2,2))  
+print(math.fmod(5.2,2))
+
+print(math.frexp(24.8))
+
+print(math.fabs(-66.43))
+print(math.fabs(-7)) 
+
+print (math.gcd(3, 6))
+print (math.gcd(6, 12))
+print (math.gcd(12, 36))
+print (math.gcd(-12, -36))
+print (math.gcd(5, 12))
+print (math.gcd(10, 0))
+print (math.gcd(0, 34))
+print (math.gcd(0, 0)) 
